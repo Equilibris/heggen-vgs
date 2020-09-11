5d13b8434c70b31f3bc002b97c3d5414a91f0afb
Equilibris <47296141+Equilibris@users.noreply.github.com>
2020-09-11 14:52:49 +0200

    py

---
+++ maths/py/into.py
@@ -0,0 +1,22 @@
+print(1+1)
+print("print e gøy")
+
+tall = 10
+
+if tall < 10:
+    print("tall er minde enn 10!")
+
+else:
+    print("tall er ikke mindre enn 10!")
+
+x = 0
+while x < 0:
+    print(x + 1)
+    x = x + 1
+
+
+
+
+
+
+
---
+++ maths/py/turtle-python/main.py
@@ -0,0 +1,33 @@
+from turtle import *
+speed((0.1))
+
+circle(100)
+
+penup()
+goto(50, 70)
+
+pendown()
+goto(-50, 70)
+
+penup()
+
+goto(-50, 120)
+
+pendown()
+
+circle(10)
+
+penup()
+
+goto(50, 120)
+
+pendown()
+
+circle(10)
+
+penup()
+goto(0,100)
+
+pendown()
+circle(5)
+done()
